9dad57d4674156f134f2b569f288402f8407886f
Narygin Kamil <saybayry@yandex.ru>
2022-10-27 14:04:40 +0700

    "добавил простую фильтрацию и часть элеменотов реализовал через bootstrap"

---
+++ storeStaffForArt/filters.py
@@ -0,0 +1,10 @@
+import django_filters
+from .models import productForArt
+
+class productForArtFilters(django_filters.FilterSet):
+    min_price = django_filters.NumberFilter(field_name="price", lookup_expr='gte')
+    max_price = django_filters.NumberFilter(field_name="price", lookup_expr='lte')
+    titleFiltrt = django_filters.Filter(field_name='title',lookup_expr='icontains')
+    class Meta:
+        model = productForArt
+        fields = ['min_price', 'max_price', 'titleFiltrt' ]
--- storeStaffForArt/views.py
+++ storeStaffForArt/views.py
@@ -2,12 +2,18 @@
 from django.views import View, generic
 from .models import productForArt
 from django.shortcuts import render
+from .filters import productForArtFilters
 
 # Create your views here.
 class startPage(generic.ListView):
     model = productForArt
     template_name = "startPage.html"
     context_object_name = "productForArt_list"
-    queryset = productForArt.objects.all()[:20]
+##    queryset = productForArt.objects.all()[:20]
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['filter'] = productForArtFilters(self.request.GET,queryset=self.get_queryset())
+        return context
+
 def pageInwork(request, *args, **kwargs):
     return  render(request, "pageInwork.html", {})
